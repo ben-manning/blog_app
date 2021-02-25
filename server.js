30344a65dc9b9dfe8307a230d00f55b19c1e0caf
Ben Manning <bmanning@me.com>
2021-02-25 15:15:10 -0500

    added views file, app index route, and app index view template

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const APP = express();
 const PORT = 3000;
 
+APP.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
 APP.listen(PORT, () => {
   console.log(`app is listening at:`, PORT)
 });
