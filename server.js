032fbed11f917b0c9e3bc70c1f8616a3bffc6745
Ben Manning <bmanning@me.com>
2021-02-25 15:19:05 -0500

    created the posts views folder and the posts/index.ejs template

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ APP.get('/', (req, res) => {
   res.render('index.ejs');
 });
 
+APP.get('/posts', (req, res) => {
+  res.render('posts/index.ejs');
+});
+
 APP.listen(PORT, () => {
   console.log(`app is listening at:`, PORT)
 });
