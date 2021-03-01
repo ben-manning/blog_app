f1772aa9ce15f9fbfea2d9cc71e25c04b40068fd
Ben Manning <bmanning@me.com>
2021-03-01 15:12:35 -0500

    adding the seed data and the index template logic

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,10 +1,42 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ROUTER = express.Router();
 
+// MODEL
+const Post = require('../models/posts.js');
+
 // POSTS
+// seed the data
+ROUTER.get('/seed', (req, res) => {
+  Post.create([
+    {
+      title: 'This is the first title',
+      body: 'This is the first body for the posts'
+    },
+    {
+      title: 'This is the second post title',
+      body: 'This is the second body for the posts'
+    },
+    {
+      title: 'This is the third post title',
+      body: 'This is the third body for the posts'
+    }
+  ], (err, createdPosts) => {
+    res.send(createdPosts);
+  })
+});
+
 // index
 ROUTER.get('/', (req, res) => {
-  res.render('posts/index.ejs');
+  Post.find({}, (err, allPosts) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.render('posts/index.ejs', {
+        posts: allPosts
+      });
+    }
+  });
 });
 
 module.exports = ROUTER;
--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const APP = express();
+const mongoose = require('mongoose');
+const db = mongoose.connection;
 
 require('dotenv').config();
 
 const PORT = process.env.PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+// Connect to Mongo
+mongoose.connect(MONGODB_URI,  { useNewUrlParser: true });
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongo not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+// open the connection to mongo
+db.on('open' , ()=>{});
+
+// MODELS
+mongoose.connect('mongodb://localhost:27017/posts', { useNewUrlParser: true});
+mongoose.connection.once('open', ()=> {
+    console.log('connected to mongo');
+});
 
 // CONTROLLERS
 const postsController = require('./controllers/posts');
